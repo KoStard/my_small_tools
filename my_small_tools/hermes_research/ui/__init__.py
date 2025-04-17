d4c0429be71665db1a16f28ef428010bd1a361b4
Ruben Kostandyan <ruben.kostard@gmail.com>
2025-04-17 15:41:49 +0200

    fix: refactor

--- my_small_tools/hermes_research/ui/__init__.py
+++ my_small_tools/hermes_research/ui/__init__.py
@@ -1,2 +1,2 @@
 # This file marks the ui directory as a Python package
-from my_small_tools.ui.menu_manager import MenuManager
+from my_small_tools.hermes_research.ui.menu_manager import MenuManager
